7658cb32c58e9ee818480afa8530889761aa2531
Will Hastings <will@objectdotcreate.net>
2014-09-28 11:41:45 -0700

    Add basic grid styling with Archetype.

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module NewsReader
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.paths << 'vendor/archetype'
   end
 end
